d128034a5c3387e7b1b4991906ba0b4fc0cc2f2c
floki <shalaby.trabajo@gmail.com>
2023-12-07 01:38:55 +0200

    p

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
-    argv = sys.argv[1:]
-    arg_num = len(argv)
-    if arg_num != 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         exit(1)
-    elif sys.argv[2] not in '+-*/':
-        print('Unknown operator. Available operators: +, -, *, and /')
-        exit(1)
+
+    a = int(argv[1])
+    sign = argv[2]
+    b = int(argv[3])
+
+    if sign == '+':
+        cal = add(a, b)
+    elif sign == '-':
+        cal = sub(a, b)
+    elif sign == '*':
+        cal = mul(a, b)
+    elif sign == '/':
+        cal = div(a, b)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] is '+':
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-        elif sys.argv[2] is '-':
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-        elif sys.argv[2] is '*':
-            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
-            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+        error = "Unknown operator. Available operators: +, -, * and /"
+        print("{}".format(error))
+        exit(1)
+
+    print("{} {} {} = {}".format(a, sign, b, cal))
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
+
 __import__("os").write("101-easy_print.py", "#pythoniscool\n".encode("UTF-8"))
